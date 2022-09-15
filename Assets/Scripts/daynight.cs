ed1cffbc1ebafd81762004e65203c95615b8fced
TIGRAN175 <43606160+TIGRAN175@users.noreply.github.com>
2022-09-15 08:15:16 +0100

    time of day update

--- Assets/Scripts/daynight.cs
+++ Assets/Scripts/daynight.cs
@@ -55,7 +55,7 @@ public class daynight : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        TimeOfDay = 200;
     }
 
     // Update is called once per frame
