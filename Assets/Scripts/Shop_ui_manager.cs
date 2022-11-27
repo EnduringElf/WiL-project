4f48e1bf49efa538eb44e5ee8e5c6a9c7e6b6280
EnduringElf <69624373+EnduringElf@users.noreply.github.com>
2022-11-27 17:02:02 +0200

    updates

--- Assets/Scripts/Shop_ui_manager.cs
+++ Assets/Scripts/Shop_ui_manager.cs
@@ -4,10 +4,31 @@ using UnityEngine;
 
 public class Shop_ui_manager : MonoBehaviour
 {
+    public GameObject Background;
+    public GameObject Template;
+    public List<Fish> fish;
+    public GameObject TemplateObjectBackground;
+    public List<GameObject> inventory_objects;
+    public float padding;
+    public GameObject player;
+
+    public float backgroundWidth;
+    public float objectwidth;
+    public float objecthieght;
+    public int WidthAmount;
+    public float originWidth;
+    public float originHieght = 410;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("player");
+        backgroundWidth = Background.GetComponent<RectTransform>().rect.width;
+        objectwidth = TemplateObjectBackground.GetComponent<RectTransform>().rect.width + 5;
+        objecthieght = TemplateObjectBackground.GetComponent<RectTransform>().rect.height + 5;
+        fish = player.GetComponent<Inventory>().InventoryFish;
     }
 
     // Update is called once per frame
@@ -15,4 +36,7 @@ public class Shop_ui_manager : MonoBehaviour
     {
         
     }
+
+
+
 }
--- Assets/Scripts/TemplateSellObject.cs
+++ Assets/Scripts/TemplateSellObject.cs
@@ -24,18 +24,18 @@ public class TemplateSellObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!thisFish)
-        {
-            Tm_Name.GetComponent<TMP_Text>().text = thisFish.Name;
-            Tm_wieght.GetComponent<TMP_Text>().text = thisFish.Wieght.ToString() + " KG";
-            Tm_value.GetComponent<TMP_Text>().text = "R " + thisFish.FinalValue.ToString();
-            Tm_Portrait.GetComponent<Image>().sprite = thisFish.Portrait;
-        }
+        //if (!thisFish)
+        //{
+        //    Tm_Name.GetComponent<TMP_Text>().text = thisFish.Name;
+        //    Tm_wieght.GetComponent<TMP_Text>().text = thisFish.Wieght.ToString() + " KG";
+        //    Tm_value.GetComponent<TMP_Text>().text = "R " + thisFish.FinalValue.ToString();
+        //    Tm_Portrait.GetComponent<Image>().sprite = thisFish.Portrait;
+        //}
     }
 
     public void SellFish()
     {
-
+        Destroy(this.gameObject);
         //return thisFish.FinalValue;
     }
 }
