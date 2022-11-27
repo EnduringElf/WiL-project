f7caa479c644ca59e28b3ecc5d0a0952444e8eb4
EnduringElf <69624373+EnduringElf@users.noreply.github.com>
2022-11-27 23:49:19 +0200

    MARKET BUG FIX

--- Assets/Scripts/Shop_ui_manager.cs
+++ Assets/Scripts/Shop_ui_manager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Shop_ui_manager : MonoBehaviour
 {
@@ -8,7 +10,7 @@ public class Shop_ui_manager : MonoBehaviour
     public GameObject Template;
     public List<Fish> fish;
     public GameObject TemplateObjectBackground;
-    public List<GameObject> inventory_objects;
+    public List<GameObject> CurrentPageObjects;
     public float padding;
     public GameObject player;
 
@@ -19,6 +21,7 @@ public class Shop_ui_manager : MonoBehaviour
     public float originWidth;
     public float originHieght = 33.8f;
     public int max_objects_per_page = 5;
+    public int current_page = 0;
 
 
 
@@ -30,25 +33,120 @@ public class Shop_ui_manager : MonoBehaviour
         objectwidth = TemplateObjectBackground.GetComponent<RectTransform>().rect.width + 5;
         objecthieght = TemplateObjectBackground.GetComponent<RectTransform>().rect.height + 5;
         fish = player.GetComponent<Inventory>().InventoryFish;
-
+        DisplayPage();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        fish = player.GetComponent<Inventory>().InventoryFish;
+
     }
 
-    public void DisplayPage(int listinterval)
+    public void DisplayPage()
     {
-        float hieght = originHieght;
 
-        for(int i = 0; i < max_objects_per_page; i++)
+        CurrentPageObjects = new List<GameObject>();
+        float hieght = originHieght;
+        int indexOrigin = current_page * max_objects_per_page;
+        for(int i = 0; i + indexOrigin < max_objects_per_page * (current_page+1); i++)
         {
+            if(indexOrigin + i < fish.Capacity)
+            {
+                if (fish[indexOrigin + i] != null)
+                {
+                    GameObject temp =
+                    Instantiate(Template);
+                    temp.transform.SetParent(Background.transform);
+                    temp.GetComponent<RectTransform>().localPosition
+                    = new Vector3(0, hieght - ((objecthieght + padding) * i), 0);
+                    CurrentPageObjects.Add(temp);
+                    temp.GetComponent<TemplateSellObject>().thisFish = fish[indexOrigin + i];
+
+                }
+            }
             
         }
 
+    }
 
+    public void DisplayPage(int currentpage)
+    {
+        fish = player.GetComponent<Inventory>().InventoryFish;
+        CurrentPageObjects = new List<GameObject>();
+        float hieght = originHieght;
+        int indexOrigin = currentpage * max_objects_per_page;
+        for (int i = 0; i + indexOrigin < max_objects_per_page * (current_page + 1); i++)
+        {
+            if (indexOrigin < 0)
+            {
+                indexOrigin = 0;
+            }
+            if (indexOrigin + i < fish.Capacity)
+            {
+                if (fish[indexOrigin + i] != null)
+                {
+                    GameObject temp =
+                    Instantiate(Template);
+                    temp.transform.SetParent(Background.transform);
+                    temp.GetComponent<RectTransform>().localPosition
+                    = new Vector3(0, hieght - ((objecthieght + padding) * i), 0);
+                    CurrentPageObjects.Add(temp);
+                    temp.GetComponent<TemplateSellObject>().thisFish = fish[indexOrigin + i];
+
+                }
+            }
+
+        }
+
+    }
+
+    public void destroyPage()
+    {
+        foreach(GameObject i in CurrentPageObjects)
+        {
+            Destroy(i);
+        }
+    }
+
+    public void NextPage()
+    {
+        current_page++;
+        int indexOrigin = current_page * max_objects_per_page;
+        if (indexOrigin < fish.Capacity && fish[indexOrigin] != null)
+        {
+            Debug.Log("showing next page");
+            destroyPage();
+            DisplayPage();
+        }
+        else
+        {
+            current_page--;
+            Debug.Log("no items in next page");
+        }
+    }
+
+    public void PrevPage()
+    {
+        current_page--;
+        int indexOrigin = current_page * max_objects_per_page;
+        if(indexOrigin < 0)
+        {
+            current_page++;
+            indexOrigin = 0;
+            Debug.Log("no items in prev page");
+        }
+        if (fish[indexOrigin] != null)
+        {
+            Debug.Log("showing prev page");
+            destroyPage();
+            DisplayPage();
+        }
+        else
+        {
+            current_page++;
+            Debug.Log("no items in prev page");
+        }
     }
 
     
